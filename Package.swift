4fe992ce961e064cb87158a7c1eaf89283d0fbd3
woolie <steven_woolgar@woolsoft.com>
2021-03-22 10:52:51 -0700

    Flailing a bit on trying to fix the manifest

--- Package.swift
+++ Package.swift
@@ -1,29 +1,18 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.4
 import PackageDescription
 
 let package = Package(
     name: "music-notation",
     platforms: [
-        // Add support for all platforms starting from a specific version.
         .macOS(.v10_15),
         .iOS(.v13),
         .tvOS(.v13),
         .watchOS(.v7)
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "music-notation",
-            targets: ["music-notation"]),
-    ],
+    	.library(name: "music-notation", targets: ["music-notation"])],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "music-notation",
-            dependencies: []),
-        .testTarget(
-            name: "music-notationTests",
-            dependencies: ["music-notation"]),
+        .target(name: "music-notation", path: "Sources", exclude: ["docs"]),
+        .testTarget(name: "music-notationTests", dependencies: ["music-notation"])
     ]
 )
