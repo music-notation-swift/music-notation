5f7397f5dcdc060872353c35bb7310f0d3dd6ca4
woolie <steven_woolgar@woolsoft.com>
2021-03-31 14:07:00 -0700

    Instrument has an initializer now. Fully public

--- Sources/music-notation/Instrument.swift
+++ Sources/music-notation/Instrument.swift
@@ -7,11 +7,23 @@
 //
 
 public struct Instrument {
-	public let name: String = ""
+	public let name: String
 
-	public let lineCount: Int = 0
+	public let lineCount: Int
 
 	/// Number of steps that this instrument is transposed.
-	public let chromaticTransposition: Int = 0
-	public let octaveTransposition: Int = 0
+	public let chromaticTransposition: Int
+	public let octaveTransposition: Int
+
+	public init(
+		name: String = "",
+		lineCount: Int = 0,
+		chromaticTransposition: Int = 0,
+		octaveTransposition: Int = 0
+	) {
+		self.name = name
+		self.lineCount = lineCount
+		self.chromaticTransposition = chromaticTransposition
+		self.octaveTransposition = octaveTransposition
+	}
 }
